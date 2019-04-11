a53002166512491ef1230b98f6ec965baea19bc5
Racquel Keli <rkeli@oa-l2.21-school.ru>
2019-04-11 15:48:10 +0300

    V 4.01

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,41 +6,30 @@
 /*   By: rkeli <rkeli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 17:35:10 by rkeli             #+#    #+#             */
-/*   Updated: 2019/04/11 13:51:02 by rkeli            ###   ########.fr       */
+/*   Updated: 2019/04/11 15:45:31 by rkeli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-
-unsigned char static		*special_for_two_line(unsigned char *s1,
-													unsigned char *s2, size_t n)
-{
-	size_t i;
-
-	i = 0;
-	i = n - 1;
-	while (n-- > 0)
-	{
-		s1[i] = s2[i];
-		i--;
-	}
-	return (s1);
-}
 
 void						*ft_memmove(void *dst, void *src, size_t n)
 {
 	unsigned char	*s1;
-	unsigned char	*s2;
 	size_t			i;
 
 	if (!dst && !src)
 		return (NULL);
 	i = 0;
 	s1 = (unsigned char*)dst;
-	s2 = (unsigned char*)src;
 	if (dst > src)
-		s1 = special_for_two_line(s1, s2, n);
+	{
+		i = n - 1;
+		while (n-- > 0)
+		{
+			s1[i] = ((unsigned char *)src)[i];
+			i--;
+		}
+	}
 	else
 	{
 		while (n-- > 0)
